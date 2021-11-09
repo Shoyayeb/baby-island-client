f4e18acbc407e6667f8666a8cc27d4af37c3693f
Shoyayeb <sa.fine@outlook.com>
2021-11-10 05:12:41 +0600

    navbar and folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
+import Footer from './Pages/Shared/Footer/Footer';
+import Navbar from './Pages/Shared/Navbar/Navbar';
+
 function App() {
   return (
     <div>
-      <p>hi</p>
+      {/* <AuthProvider> */}
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          {/* <Route path="/login">
+              <Login></Login>
+            </Route>
+            <Route path="/register">
+              <Register></Register>
+            </Route>
+            <PrivateRoute path="/onlinedoctor">
+              <OnlineDoctor></OnlineDoctor>
+            </PrivateRoute>
+            <PrivateRoute path="/ambulance">
+              <Ambulance></Ambulance>
+            </PrivateRoute>
+            <PrivateRoute path="/service/:serviceId">
+              <Service></Service>
+            </PrivateRoute> */}
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+      {/* </AuthProvider> */}
     </div>
   );
 }
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <p>this is not home right?</p>
+        </div>
+    );
+};
+
+export default Home;
