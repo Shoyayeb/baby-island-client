a9873c1da603861b8717a5d358d2025092e6f556
Shoyayeb <sa.fine@outlook.com>
2021-11-19 20:31:31 +0600

    add review

--- src/Pages/DashBoard/Pay/Pay.js
+++ src/Pages/DashBoard/Pay/Pay.js
@@ -1,25 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Pay = (props) => {
     return (
-        <div>
-            <div><p className="text-6xl">hi</p>
-                <div className="bg-red-500 flex items-center text-white text-sm font-bold px-4 py-3 relative w-full" role="alert">
-                    <svg width="20" height="20" fill="currentColor" className="w-4 h-4 mr-2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M1216 1344v128q0 26-19 45t-45 19h-512q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h64v-384h-64q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h384q26 0 45 19t19 45v576h64q26 0 45 19t19 45zm-128-1152v192q0 26-19 45t-45 19h-256q-26 0-45-19t-19-45v-192q0-26 19-45t45-19h256q26 0 45 19t19 45z">
-                        </path>
-                    </svg>
-                    <p>
-                        HIii
-                    </p>
-                    <button className="absolute top-0 bottom-0 right-0 px-4 py-3" >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="h-6 w-6 text-white" viewBox="0 0 1792 1792">
-                            <path d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z">
-                            </path>
-                        </svg>
-                    </button>
-                </div></div>
+
+        <div class="bg-white dark:bg-gray-800 ">
+            <div class="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
+                <h2 class="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
+                    <span class="block">
+                        Payment system coming soon
+                    </span>
+
+                </h2>
+                <div class="lg:mt-0 lg:flex-shrink-0">
+                    <div class="mt-12 inline-flex rounded-md shadow">
+                        <Link to="/home" class="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                            Go Home
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 };
 
