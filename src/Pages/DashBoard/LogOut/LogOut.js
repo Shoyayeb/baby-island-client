0eaefa68eedb300587c69a29531d46feec56c4ab
Shoyayeb <sa.fine@outlook.com>
2021-11-19 21:44:23 +0600

    log out

--- src/Pages/DashBoard/LogOut/LogOut.js
+++ src/Pages/DashBoard/LogOut/LogOut.js
@@ -1,9 +1,16 @@
+import { LogoutIcon } from '@heroicons/react/outline';
 import React from 'react';
+import useAuth from '../../../hooks/useAuth';
 
 const LogOut = () => {
+    const { user, logOut } = useAuth();
     return (
-        <div>
-            <p>hi</p>
+        <div className="flex items-center justify-center">
+            <button onClick={() => { logOut() }} class=" px-6 py-2  transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none">
+                <LogoutIcon className=" md:h-10 md:w-10" />
+                Log Out
+            </button>
+
         </div>
     );
 };
